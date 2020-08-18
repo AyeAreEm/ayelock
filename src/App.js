5243f12e1ecac1a8ec9865cea69be1ba1fe9fbf3
AyeAreEm <ag.ayeareem@gmail.com>
2020-08-18 21:32:13 +0800

    new file:   public/AyeLock.png 	deleted:    public/robots.txt 	modified:   src/App.js

--- src/App.js
+++ src/App.js
@@ -4,6 +4,11 @@ import Generator from './Generator';
 import './App.css';
 
 function App() {
+  const image = {
+    width: "340px",
+    height: "110px"
+  }
+
   const [weak, setWeak] = useState(Generator(true, true, true, true, 12));
   const [rec, setRec] = useState(Generator(true, true, true, true, 15));
   const [strong, setStrong] = useState(Generator(true, true, true, true, 18));
@@ -20,24 +25,20 @@ function App() {
     });
   }, []);
 
-  const refresh = () => {
-    setWeak(Generator(true, true, true, true, 12));
-    setRec(Generator(true, true, true, true, 15));
-    setStrong(Generator(true, true, true, true, 18));
-    setEasy(Generator(true, true, false, false, 16));
-  }
-
   return (
     <div className="App">
-      <h1>AyeLock</h1>
-      <button onClick={refresh}>Refresh</button>
-      <br/>
+      <img src="./AyeLock.png" alt="AyeLock" title="AyeLock" style={image}/>
+      <br/><br/>
+
       <Password type="Weak:" password={weak}/>
       <br/>
+
       <Password type="Recommended:" isRed={true} password={rec}/>
       <br/>
+
       <Password type="Strong:" password={strong}/>
       <br/>
+
       <Password type="Easy to read:" isRed={true} password={easy}/>
     </div>
   );
