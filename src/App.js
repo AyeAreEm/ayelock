4ecf7dbdf2a006878a43ec0c85a93aa664ad68c8
AyeAreEm <ag.ayeareem@gmail.com>
2020-08-18 20:48:30 +0800

    modified:   public/index.html 	modified:   src/App.css 	modified:   src/App.js 	modified:   src/Password.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import Password from './Password.js';
 import Generator from './Generator';
 import './App.css';
 
 function App() {
-  let weak = Generator(true, true, true, true, 12);
-  let rec = Generator(true, true, true, true, 15);
-  let strong = Generator(true, true, true, true, 18);
-  let easy = Generator(true, true, false, false, 16);
+  const [weak, setWeak] = useState(Generator(true, true, true, true, 12));
+  const [rec, setRec] = useState(Generator(true, true, true, true, 15));
+  const [strong, setStrong] = useState(Generator(true, true, true, true, 18));
+  const [easy, setEasy] = useState(Generator(true, true, false, false, 16));
+
+  useEffect(() => {
+    window.addEventListener('keyup', e => {
+      if (e.keyCode === 82) {
+        setWeak(Generator(true, true, true, true, 12));
+        setRec(Generator(true, true, true, true, 15));
+        setStrong(Generator(true, true, true, true, 18));
+        setEasy(Generator(true, true, false, false, 16));
+      }
+    });
+  }, []);
+
+  const refresh = () => {
+    setWeak(Generator(true, true, true, true, 12));
+    setRec(Generator(true, true, true, true, 15));
+    setStrong(Generator(true, true, true, true, 18));
+    setEasy(Generator(true, true, false, false, 16));
+  }
 
   return (
     <div className="App">
       <h1>AyeLock</h1>
+      <button onClick={refresh}>Refresh</button>
       <br/>
       <Password type="Weak:" password={weak}/>
       <br/>
-      <Password type="Recommended:" password={rec}/>
+      <Password type="Recommended:" isRed={true} password={rec}/>
       <br/>
       <Password type="Strong:" password={strong}/>
       <br/>
-      <Password type="Easy to read:" password={easy}/>
+      <Password type="Easy to read:" isRed={true} password={easy}/>
     </div>
   );
 }
--- src/Password.js
+++ src/Password.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
 
-const Password = ({type, password}) => {
+const Password = ({type, isRed, password}) => {
+
     return (
         <div className="PasswordBox">
-            <h2>{type}</h2>
+            <h2><span className={isRed ? "red" : ''}>{type}</span></h2>
             <h2>{password}</h2>
         </div>
     );
